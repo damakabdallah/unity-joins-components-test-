8a96c07d51890ae591e6daa99a1264ecceeb2492
Damak Abdallah <xttrzax@gmail.com>
2018-08-01 13:48:59 +0100

    the first commit

---
+++ Assets/changeDirection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeDirection : MonoBehaviour {
+    public SurfaceEffector2D SE2D;
+	// Use this for initialization
+	void Start () {
+        SE2D = this.GetComponent<SurfaceEffector2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SE2D.speed *= -1;
+	}
+}
---
+++ Assets/rotatePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatePlayer : MonoBehaviour {
+    public GameObject player;
+    public ConstantForce2D CF2D;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        CF2D = player.GetComponent<ConstantForce2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.transform.position.x > -1 && player.transform.position.x < 1 && player.transform.position.y > -1 && player.transform.position.y < 1)
+            CF2D.torque = 6500;
+        else
+            CF2D.torque = 0;
+        
+	}
+}
